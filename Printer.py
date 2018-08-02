fb75e150f2e29927fc0818c5f7d258ee3f34f78b
Kamer Koçak <31200856+kamerkocak@users.noreply.github.com>
2018-08-02 13:31:44 +0300

    Update Printer.py

--- Printer.py
+++ Printer.py
@@ -1,5 +1,8 @@
 from DollarTry import *
 from EuroTry import *
+from SterlingTry import *
+
 def showMe():
     print("USD/TRY: " + str(getDollarTryRate()) + " As Of : " + str(getDolarRefTime()) + " UTC (tsi -3)")
     print("EUR/TRY: " + str(getEuroTryRate()) + " As Of : " + str(getEuroRefTime()) + " UTC (tsi -3)")
+    print("GBP/TRY: " + str(getSterlingTryRate()) + " As Of : " + str(getSterlingRefTime()) + " UTC (tsi -3)")
